dbbd873136daa3da4ccfabb02cb4c61408572455
robmckinnon <rob@theyworkforyou.co.nz>
2011-06-02 17:26:29 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Expertgroups::Application.config.secret_token = '7faf85d4a28bd5995709b988f67fa45a8c9bfc2951e5a017563a429ccc95e5aed457caedb07884ae0aa6e597eed7658d1d5448ed02f107ebfdf82e7a5e98f261'
