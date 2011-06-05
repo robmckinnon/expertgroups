4be3c0667828424dc8a5c688916fea74abb9b35b
robmckinnon <rob@theyworkforyou.co.nz>
2011-06-05 21:38:51 +0100

    change migration to run only in dev

--- db/migrate/20110605133628_increase_content_text_limit.rb
+++ db/migrate/20110605133628_increase_content_text_limit.rb
@@ -1,7 +1,9 @@
 class IncreaseContentTextLimit < ActiveRecord::Migration
   def self.up
-    change_column :wiki_pages, :content, :text, :limit => 65537
-    change_column :wiki_page_versions, :content, :text, :limit => 65537
+    if Rails.env == 'development'
+      change_column :wiki_pages, :content, :text, :limit => 65537
+      change_column :wiki_page_versions, :content, :text, :limit => 65537
+    end
   end
 
   def self.down
